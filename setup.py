fc62486f835ebc7a959e9d3c9406be306bb05b73
imgurbot12 <imgurbot12@gmail.com>
2023-04-23 17:17:18 -0700

    chore: downgrade version for first release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='pydhcp3',
-    version='0.0.2',
+    version='0.0.1',
     license='MIT',
     author='Andrew Scott',
     author_email='imgurbot12@gmail.com',
