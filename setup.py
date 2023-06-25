7bf178f3af32528041b8160db2da68da939ea920
imgurbot12 <imgurbot12@gmail.com>
2023-06-25 01:07:40 -0700

    chore: upgrade version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='pydhcp3',
-    version='0.0.3',
+    version='0.0.4',
     license='MIT',
     author='Andrew Scott',
     author_email='imgurbot12@gmail.com',
